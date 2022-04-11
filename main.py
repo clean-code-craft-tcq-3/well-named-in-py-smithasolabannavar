e59d41583beba90741fd7c5569a311d0e6bc9e97
S Solabannavar Smitha <SSolabannavar.Smitha@in.bosch.com>
2022-04-11 22:48:47 +0530

    Moved constants to seperate files

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from get_color_from_pair_number import get_pair_number_from_color, get_color_from_pair_number
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+from get_color_and_pair_number import get_pair_number_from_color, get_color_from_pair_number
+import constants
 
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
-
 
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
@@ -12,11 +10,9 @@ def test_number_to_pair(pair_number,
   assert(major_color == expected_major_color)
   assert(minor_color == expected_minor_color)
 
-
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
-
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
